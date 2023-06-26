ee5eed4cde6c1660d4b6356e9e2c80d889a3e766
Diogo <you@example.com>
2023-06-26 15:28:42 -0300

    Part 1 do projeto da Udemy -Keeper App no React-

---
+++ src/components/note.jsx
@@ -0,0 +1,8 @@
+function Note(){
+    return <div className="note">
+        <h1 className="note">Titulo</h1>
+        <p className="note">Texto</p>
+    </div>
+}
+
+export default Note
--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
-//1. Create a new React app.
-//2. Create a App.jsx component.
+//1. Create a new React app.-ok
+//2. Create a App.jsx component.-ok
 //3. Create a Header.jsx component that renders a <header> element
-//to show the Keeper App name in an <h1>.
+//to show the Keeper App name in an <h1>.-ok
 //4. Create a Footer.jsx component that renders a <footer> element
-//to show a copyright message in a <p> with a dynamically updated year.
+//to show a copyright message in a <p> with a dynamically updated year.-ok
 //5. Create a Note.jsx component to show a <div> element with a
-//<h1> for a title and a <p> for the content.
+//<h1> for a title and a <p> for the content.-ok
 //6. Make sure that the final website is styled like the example shown here:
 //https://l1pp6.csb.app/
 
 //HINT: You will need to study the classes in teh styles.css file to appy styling.
+
+import react from 'react'
+import {createRoot} from 'react-dom/client'
+import Footer from './components/footer'
+import Header from './components/header'
+import App from './components/App'
+import Note from './components/note'
+import './public/styles.css'
+
+const rootElement=  document.getElementById('root')
+const root= createRoot(rootElement)
+
+root.render(
+    <react.StrictMode>
+        <App/>
+        <Header/>
+        <Note/>
+        <Footer/>
+    </react.StrictMode>
+)
