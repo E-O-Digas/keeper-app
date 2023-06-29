027e8a6ad6ecad3848e4a1f7129060a4da68d4a3
Diogo <you@example.com>
2023-06-29 15:57:16 -0300

    Teste Kepper App v3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,20 +2,14 @@ import React from "react"
 import Note from './Note'
 import Header from './header'
 import Footer from './footer'
-import notes from '../notes'
+import Input from './input'
 
 
 function App(){
     return(
         <>
             <Header/>
-            {notes.map(noteItems => 
-                <Note 
-                    key={noteItems.key} 
-                    title={noteItems.title} 
-                    content={noteItems.content}
-                />
-            )}
+            <Input/>
             <Footer/>
         </>
     )
